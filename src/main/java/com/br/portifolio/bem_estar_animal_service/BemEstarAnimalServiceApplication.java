89cff962dd0a11c1684ce8db7ec1bcf8f7d203f0
Willy <willydasilva29@gmail.com>
2025-11-28 00:25:19 -0300

    first commit

---
+++ src/main/java/com/br/portifolio/bem_estar_animal_service/BemEstarAnimalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.portifolio.bem_estar_animal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemEstarAnimalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemEstarAnimalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/portifolio/bem_estar_animal_service/BemEstarAnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.portifolio.bem_estar_animal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemEstarAnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
